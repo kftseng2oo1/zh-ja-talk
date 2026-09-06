2763a87df34d1d2e6c09a49f14604776e9dc8342
kftseng2oo1 <kftseng2oo1@gmail.com>
2026-09-06 22:12:10 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'zh-ja-talk-v3';
+var CACHE = 'zh-ja-talk-v6';
 var ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', function(e){
   e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(ASSETS); }).then(function(){ return self.skipWaiting(); }));
